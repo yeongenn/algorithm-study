86fb6a09eb030667da7378a0f719aa8b6aed3274
yeongenn <yngn0012@naver.com>
2025-01-25 23:41:33 +0900

    BOJ 2480 주사위 세개

--- BOJ/Solution2480.java
+++ BOJ/Solution2480.java
@@ -13,19 +13,18 @@ public class Solution2480 {
 
         int rewards = 0;
 
-        /*
-         * 같은 눈이 3개가 나오면 10,000원+(같은 눈)×1,000원의 상금
-         * 같은 눈이 2개만 나오는 경우에는 1,000원+(같은 눈)×100원의 상금
-         * 모두 다른 눈이 나오는 경우에는 (그 중 가장 큰 눈)×100원의 상금
-         */
+        if (A == B && B == C) {
+            rewards = 10000 + A * 1000;
+        } else if (A == B || A == C) {
+            rewards = 1000 + A * 100;
+        } else if (B == C) {
+            rewards = 1000 + B * 100;
+        } else {    // 주사위 눈 모두 다를 경우
+            int m = Math.max(A, Math.max(B, C));
+            rewards = m * 100;
+        }
 
-        // if(A == B && B == C && C == A){
-        // rewards = 10000 + (A * 1000);
-        // } else if (A != B && B != C && C != A) {
-        // int biggest =
-        // rewards =
-        // }
-
+        System.out.println(rewards);
     }
 
 }
