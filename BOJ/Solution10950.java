3da7021c54c4feb953de034b977eced157835106
yeongenn <yngn0012@naver.com>
2025-01-27 17:58:21 +0900

    BOJ 10950 A + B - for loop

---
+++ BOJ/Solution10950.java
@@ -0,0 +1,26 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution10950 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append(a + b).append("\n");
+        }
+
+        System.out.println(sb.toString());
+    }
+
+}
