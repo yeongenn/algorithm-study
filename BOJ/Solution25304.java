94e2649509c8f807589260250b19bd872853678c
yeongenn <yngn0012@naver.com>
2025-01-28 23:27:47 +0900

    BOJ 25304 영수증

---
+++ BOJ/Solution25304.java
@@ -0,0 +1,27 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class Solution25304 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int total = sc.nextInt();
+        int T = sc.nextInt();
+
+        int sum = 0;
+
+        for (int i = 0; i < T; i++) {
+            int price = sc.nextInt();
+            int count = sc.nextInt();
+            sum += price * count;
+
+        }
+
+        if (sum == total) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+
+}
