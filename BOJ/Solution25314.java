bf16874e4e67414d98d4dc138fa68dd35058e326
yeongenn <yngn0012@naver.com>
2025-01-28 23:28:27 +0900

    BOJ 25314 코딩은 체육과목이 아닙니다

---
+++ BOJ/Solution25314.java
@@ -0,0 +1,19 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class Solution25314 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        int lCount = T / 4;
+        for(int i = 0; i < lCount; i++){
+            System.out.print("long ");
+        }
+
+        System.out.print("int");
+    }
+    
+}
