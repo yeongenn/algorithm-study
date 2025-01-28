301ff1da24a382f208c6bc48c7b7747d8f71f880
yeongenn <yngn0012@naver.com>
2025-01-28 23:28:55 +0900

    BOJ 11021, 11022 A + B

---
+++ BOJ/Solution11021.java
@@ -0,0 +1,21 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class Solution11021 {
+
+    // A + B - (7), A + B - (8)
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        for(int i = 0; i < T; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            // System.out.printf("Case #%d: %d%n", i + 1, a + b);   // (7)
+            System.out.printf("Case #%d: %d + %d = %d%n", i + 1, a, b, a + b);  // (8)
+        }
+    
+    }
+    
+}
