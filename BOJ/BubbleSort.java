8625ece3c3fb9df4ab8ae543436e3e925b789abb
yeongenn <yngn0012@naver.com>
2025-02-05 17:48:04 +0900

    버블 정렬 index 다른 버전

---
+++ BOJ/BubbleSort.java
@@ -0,0 +1,30 @@
+package BOJ;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    
+    public static void main(String[] args) {
+        
+        // 1 ~ 100 사이 자연수 랜덤 배열 생성
+        int[] arr = new int[10];
+        for(int i = 0 ; i < arr.length ; i++){
+            arr[i] = (int) (Math.random() * 100) + 1;
+        }
+
+        System.out.println("Before : " + Arrays.toString(arr));
+
+        // 버블 정렬
+        for(int i = arr.length; i > 0; i--){
+            for(int j = 0; j < i - 1; j++){
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        
+        System.out.println("After : " + Arrays.toString(arr));        
+    }
+}
