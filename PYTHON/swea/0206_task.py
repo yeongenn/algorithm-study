c0d0bfac41767ad34c628f20e546c684ce5975df
yeongenn <yngn0012@naver.com>
2025-02-07 17:02:42 +0900

    SWEA List - 1 풀이 중

--- PYTHON/swea/0206_task.py
+++ PYTHON/swea/0206_task.py
@@ -44,19 +44,77 @@
 
 #########################################################################################################
 
-# 전기버스
+# # 전기 버스
 
 # T = int(input())
+# for t in range(T):
+#     # K : 이동 가능한 최대 정류장 수
+#     # N : 종점
+#     # M : 설치된 충전기 수
+#     K, N, M = map(int, input().split())
 
-# K : 최대 이동 정류장 수
-# N : 종점 정류장
-# M : 충전기 설치된 정류장 수
-K, N, M = map(int, input().split())
+#     stt_loc = list(map(int, input().split()))
+#     stt_loc.append(0)
 
-stt_loc = list(map(int, input().split()))
+#     move = K
+#     fill = 0
+#     fill_loc = []  # 확인용
+
+#     for i in range(M):
+#         if i == 0:
+#             move -= stt_loc[i]
+#         else:
+#             move -= stt_loc[i] - stt_loc[i - 1]
+
+#         if i == (M - 1):
+#             if stt_loc[i] + move >= N:
+#                 break
+#             else:
+#                 fill += 1
+#                 # fill_loc.append(stt_loc[i])
+#                 move = K
+
+#         if stt_loc[i] + move >= stt_loc[i + 1]:
+#             continue
+#         else:
+#             fill += 1
+#             # fill_loc.append(stt_loc[i])
+#             move = K
+#             if stt_loc[i] + move < stt_loc[i + 1]:
+#                 fill = 0
+#                 break
 
 
+#     # print(fill)
+#     # print(fill_loc)
+
+#     print(f'#{t + 1} {fill}')
+
+#########################################################################################################
+
+# 간단한 소인수 분해
+
+T = int(input())
+dv = [2, 3, 5, 7, 11]
+
+for t in range(T):
+    dv_count = [0] * 12
+    N = int(input())
     
+    for num in dv:
+        while N % num == 0:
+            dv_count[num] += 1
+            N /= num
+
+    result = []
+    for num in dv:
+        result.append(dv_count[num])
+
+    # print(result)
+
+    print(f'#{t + 1}', *result)
+
+#########################################################################################################
     
     
         
