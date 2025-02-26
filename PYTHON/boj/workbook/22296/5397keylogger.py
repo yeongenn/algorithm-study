087d3384d998e801c6581645c8194d10f89699df
yeongenn <yngn0012@naver.com>
2025-02-26 21:24:10 +0900

    to solve

---
+++ PYTHON/boj/workbook/22296/5397keylogger.py
@@ -0,0 +1,31 @@
+import sys
+sys.stdin = open("C:\\SSAFY\\algo-python\\PYTHON\\boj\\input.txt", "r")
+
+T = int(input())
+
+for _ in range(T):
+    logged = list(input())
+    # print(logged)
+    
+    password = []
+    idx = 0
+    for char in logged:
+        if char == "<":
+            if idx > 0:
+                idx -= 1
+        elif char == ">":
+            if idx < len(password):
+                idx += 1
+        elif char == "-":
+            if idx > 0:
+            # password.pop()
+                password.pop(idx - 1)
+                idx -= 1
+        else:
+            # password.append(char)
+            password.insert(idx, char)
+            idx += 1
+    print(''.join(password))
+    
+    # LinkedList로 해보기
+    # https://hyeinisfree.tistory.com/70
