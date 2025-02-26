cf02b9c8ec286ea229fd4e8d331ddf4975bf5900
yeongenn <yngn0012@naver.com>
2025-02-26 17:55:08 +0900

    boj 2503 solved

--- PYTHON/boj/2503numberbaseball.py
+++ PYTHON/boj/2503numberbaseball.py
@@ -5,7 +5,6 @@ sys.stdin = open("C:\\Users\\SSAFY\\Desktop\\YH\\algo-python\\PYTHON\\boj\\input
 
 # 후보군 123 ~ 987
 poss = [p for p in permutations([1, 2, 3, 4, 5, 6, 7, 8, 9], 3)]
-candidat = []
 
 N = int(input())
 
@@ -13,10 +12,20 @@ for _ in range(N):
     number, st, b = map(int, input().split())
     # number to list
     number = list(map(int, str(number)))
-    print(number)
+    # print(number)
+    candidat = []
     for p in poss:
         cnt_s, cnt_b = 0, 0 # 스트 볼 카운트 초기화
-        # if p[0] == number[0] and p[1] != number[1] and p[2] != number[2]:
-        #     cnt_s += 1
         
-        pass
+        for i in range(3):
+            if p[i] == number[i]:   # 스트
+                cnt_s += 1
+            if p[i] != number[i] and number[i] in p:
+                cnt_b += 1  # 볼
+        
+        if cnt_s == st and cnt_b == b:
+            candidat.append(p)
+
+    poss = candidat[:]
+
+print(len(poss))
