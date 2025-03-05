a9e689016c6be2c07dd5a221a4fb7051eaa6afcb
yeongenn <yngn0012@naver.com>
2025-03-06 00:39:49 +0900

    boj 10157 - double list

--- PYTHON/boj/10157seat.py
+++ PYTHON/boj/10157seat.py
@@ -1,42 +1,32 @@
-# import sys
-# sys.stdin = open("input.txt", "r")
-# from pprint import pprint
-
-# 범위 이내일 때, arr가 0일 때 나아간다
-# else: 방향 전환
-# cnt가 n**2 이내일 때까지만
+import sys
+sys.stdin = open("c:\\SSAFY\\algo-python\\PYTHON\\boj\\input.txt", "r")
 
 N, M = map(int, input().split())
 T = int(input())
-arr = [[0]*N for _ in range(M)]
+arr = [[0] * N for _ in range(M)]
 
-dy = [-1, 0, 1, 0]
-dx = [0, 1, 0, -1]
+dy, dx = [-1, 0, 1, 0], [0, 1, 0, -1]   # 상우하좌
 
-y, x, cnt, d = M-1, 0, 1, 0
-arr[y][x] = cnt
-ry, rx = 0, 0
+y, x = M, 0     # 시작점
+k = 0       # 좌석 번호
+d = 0       # 초기 방향
+ry, rx = 0, 0   # T의 좌표
 
-while cnt < N*M:
+if T > N * M:
+    print(0)
+    exit()      # 번호가 좌석 범위 밖이면 종료
+
+while k < N * M:
     ny = y + dy[d]
     nx = x + dx[d]
     if 0 <= ny < M and 0 <= nx < N and arr[ny][nx] == 0:
-        cnt += 1
-        arr[ny][nx] = cnt
-        y = ny
-        x = nx
-        if cnt == T:
-            ry = ny
-            rx = nx
-    else:
-        d = (d+1) % 4
+        k += 1
+        arr[ny][nx] = k
+        y, x = ny, nx
+        if k == T:
+            ry, rx = ny, nx
+    else:   # 범위 밖이거나 이미 배정되었다면
+        d = (d + 1) % 4     # 방향 전환
 
-if T > N*M:
-    print(0)
 else:
-    print(rx, ry)
-    print(rx+1, M-ry + 1)
-
-
-
-
+    print(rx + 1, M - ry)
