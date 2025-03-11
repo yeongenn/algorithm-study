c27e37e93abb4793fdf0a3ed67d5dd7a5b810ecf
yeongenn <yngn0012@naver.com>
2025-03-11 12:06:30 +0900

    swea tree inorder - solved in python

--- PYTHON/swea/Tree/inorder.py
+++ PYTHON/swea/Tree/inorder.py
@@ -3,16 +3,25 @@ sys.stdin = open("c://Users//SSAFY//Desktop//YH//algo-python//PYTHON//swea//inpu
 
 T = 2
 
-def read_word():
-    pass
+def read_word(n):   # 중간 노드
+    if n <= N:
+        read_word(n * 2)        # left
+        print(tree[n], end="")
+        read_word(n * 2 + 1)    # right
 
 for t in range(T):
     N = int(input())
-    tree = ['' for _ in range(N)]
+    tree = ['' for _ in range(N + 1)]
     
     for _ in range(N):
-        i, v, l, r = input().split()
-        tree[int(i)] = v
+        # i, v, l, r = input().split()
+        # tree[int(i)] = v
+        temp = list(input().split())
+        tree[int(temp[0])] = temp[1]
+        
     print(tree)
     
-    print(f'#{t + 1}')
+    print(f'#{t + 1}', end=" ")
+    read_word(1)
+    print()
+    
