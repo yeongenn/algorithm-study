77446dfbc5c49b658e9e4a4b8ac8ffd1732ee161
yeongenn <yngn0012@naver.com>
2025-03-13 20:58:47 +0900

    swea club - brute force & backtracking

--- PYTHON/swea/BruteForce&BackTracking/1486shelf.py
+++ PYTHON/swea/BruteForce&BackTracking/1486shelf.py
@@ -1,38 +1,27 @@
+# import sys
+# sys.stdin = open("c://Users//SSAFY//Desktop//YH//algo-python//PYTHON//swea//input.txt", "r")
+
 import sys
-sys.stdin = open("c://Users//SSAFY//Desktop//YH//algo-python//PYTHON//swea//input.txt", "r")
+sys.stdin = open("c://SSAFY//algo-python//PYTHON//swea//input.txt", "r")
 
 T = int(input())
 
 for t in range(T):
     N, B = map(int, input().split())    # N : 점원 수, B : 탑 높이
     clerks = list(map(int, input().split()))
-    print(clerks)
+    # print(clerks)
     
     temp = []
-    min_value = B
+    min_value = 200001
     
-    def subset(n, cnt, total):
-        global min_value
+    for i in range(1<<len(clerks)):
+        subset = []
+        for j in range(len(clerks)):
+            if i & (1<<j):
+                subset.append(clerks[j])
+                if sum(subset) >= B:
+                    min_value = min(min_value, sum(subset))
         
-        if total >= B:
-            print(total)
-            min_value = min(min_value, total - B)
-            return
-        
-        if n == cnt:
-            # if total >= B:
-            #     print(total)
-            #     min_value = min(min_value, total - B)
-            return
-        
-        for num in clerks:
-            temp.append(num)
-            subset(n, cnt + 1, total + num)
-            temp.pop()
+    print(f'#{t + 1} {min_value - B}')
     
-    
-    for i in range(1, N + 1):
-        subset(i, 0, 0)
-        temp = []               # 리셋
-        
-    print(f'#{t + 1} {min_value}')
+# https://velog.io/@94applekoo/%EB%B9%84%ED%8A%B8%EC%97%B0%EC%82%B0%EC%9E%90%EB%A1%9C-%EB%B6%80%EB%B6%84-%EC%A7%91%ED%95%A9%EC%9D%84-%EC%83%9D%EC%84%B1%ED%95%98%EB%8A%94-%EB%B2%95-python
--- PYTHON/swea/BruteForce&BackTracking/2806N-Queen.py
+++ PYTHON/swea/BruteForce&BackTracking/2806N-Queen.py
@@ -1,37 +1,36 @@
 import sys
-sys.stdin = open("c://Users//SSAFY//Desktop//YH//algo-python//PYTHON//swea//input.txt", "r")
-
-import copy
+sys.stdin = open("c://SSAFY//algo-python//PYTHON//swea//input.txt", "r")
 
 T = int(input())
 
-def next(idx, n):   # idx : 이전에 퀸 뒀던 자리 열 인덱스, n : 행 인덱스
+def next(n):
     global cnt
-    if n == N - 1:
+    if n == N:
         cnt += 1
         return 
                 
-    for j in range(N):
-        if idx == j:
+    for j in range(N):        
+        if checked[j]:
             continue
         
-        if abs(idx - j) == 1:
-            continue
-        
-        if not queen[j]:
-            queen[j] = 1
-            next(j, n + 1)
-            queen[j] = 0
+        valid_in_diag = True
+        for k in range(n):
+            if abs(k - n) == abs(queens[k] - j): # 행과 열의 차이가 같으면 대각선 상
+                valid_in_diag = False
+                break
+            
+        if valid_in_diag:
+            queens[n] = j    # 현재 행의 퀸 위치 마킹
+            checked[j] = 1    # 체크 마킹
+            next(n + 1)
+            checked[j] = 0    # 해제
 
 for t in range(T):
     N = int(input())
-    # board = [[0] * N for _ in range(N)]
-    queen = [0] * N
+    checked = [0] * N   # 마킹 배열 없으니까 대각선 부분에서 문제
+    queens = [99] * N   # 하나의 열에 퀸은 하나씩만 존재 가능 -> 1차원 리스트로도 가능
     cnt = 0
     
-    for i in range(N):
-        queen[i] = 1        # 마킹 
-        next(i, 0)
-        queen[i] = 0
+    next(0)
                      
     print(f'#{t + 1} {cnt}')
