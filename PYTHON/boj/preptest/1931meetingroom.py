03329b801e36c8b806312c97c7560a1ff9917327
yeongenn <yngn0012@naver.com>
2025-03-31 23:01:28 +0900

    boj problem 1931 - 3회차 월말평가 대비

--- PYTHON/boj/preptest/1931meetingroom.py
+++ PYTHON/boj/preptest/1931meetingroom.py
@@ -1,6 +1,6 @@
 import sys
-sys.stdin = open("C:\\Users\\SSAFY\\Desktop\\YH\\algo-python\\PYTHON\\boj\\input.txt", "r")
-# sys.stdin = open("C:\\SSAFY\\algo-python\\PYTHON\\boj\\input.txt", "r")
+# sys.stdin = open("C:\\Users\\SSAFY\\Desktop\\YH\\algo-python\\PYTHON\\boj\\input.txt", "r")
+sys.stdin = open("C:\\SSAFY\\algo-python\\PYTHON\\boj\\input.txt", "r")
 
 # 3회차 월말 평가 대비
 
@@ -43,18 +43,32 @@ schedules = [list(map(int, input().split())) for _ in range(N)]
 # 정렬?
 # 똑같이 시간 초과 나잖아~!~!~!~!
 
-schedules.sort(key=lambda x : (x[1], x[0]))     #  정렬
+schedules.sort(key=lambda x : (x[1], x[0]))     #  정렬, 우선순위대로 튜플에 설정
 # print(schedules)
 n = len(schedules)
-max_value = 0
 
-for i in range(n):
-    count = 1   # 사용 횟수 초기화
-    prev = schedules[i][1]  # 이전 회의 종료 시간 관리
-    for j in range(i + 1, n):
-        if schedules[j][0] >= prev:
-            count += 1
-            prev = schedules[j][1]      # prev 갱신
-    max_value = max(max_value, count)
+# for i in range(n):
+#     count = 1   # 사용 횟수 초기화
+#     prev = schedules[i][1]  # 이전 회의 종료 시간 관리
+#     for j in range(i + 1, n):
+#         if schedules[j][0] >= prev:
+#             count += 1
+#             prev = schedules[j][1]      # prev 갱신
+#     max_value = max(max_value, count)
 
-print(max_value)
+# 정렬 후 첫번째 회의는 무조건 배정
+prev = schedules[0][1]  # 이전 회의 종료 시간 관리
+count = 1
+for i in range(1, n):
+    if schedules[i][0] >= prev:
+        count += 1
+        prev = schedules[i][1]
+    
+print(count)
+
+###############################################################################################
+# 첫번째 회의를 반드시 선택하지 않아도 같은 최적해 구할 수 있는데?
+
+# 그리디 알고리즘에서는 항상 최적해 보장하는 여러 가지 선택지가 존재
+# 중요한 것은 빨리 끝나는 회의를 우선적으로 선택하면 최적해 유지가 가능하다는 점
+# -> 여러 개의 최적해가 존재하더라도 빨리 끝나는 회의를 선택하는 전략이 항상 최적해로 이어진다
