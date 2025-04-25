480e26cbc6809cdbd688702852e2fa70b5856c48
yeongenn <yngn0012@naver.com>
2025-04-25 21:53:46 +0900

    prep for im

--- PYTHON/boj/workbook/7091/2564securityguard.py
+++ PYTHON/boj/workbook/7091/2564securityguard.py
@@ -1,3 +1,3 @@
 import sys
-sys.stdin = open("c://Users//SSAFY//Desktop//YH//algo-python//PYTHON//boj//input.txt", "r")
-# sys.stdin = open("c://SSAFY//algo-python//PYTHON//boj//input.txt", "r")
+# sys.stdin = open("c://Users//SSAFY//Desktop//YH//algo-python//PYTHON//boj//input.txt", "r")
+sys.stdin = open("c://SSAFY//algo-python//PYTHON//boj//input.txt", "r")
