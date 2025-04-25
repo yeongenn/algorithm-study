8cdf568a987fb9c9e581435b146933bf6949e8a0
yeongenn <yngn0012@naver.com>
2025-04-25 22:05:48 +0900

    250422 test review - a

---
+++ PYTHON/swea/Test/a/a_test_250422.py
@@ -0,0 +1,26 @@
+# 250422 A형 문제 리뷰
+
+# 다익스트라 기본 코드로 해결
+
+"""
+2
+5, K    # K도 있었던 것 같은데 기억이 안남,,
+1 1 1 1 1
+1 1 1 1 1
+1 1 1 1 1
+1 1 1 1 1
+1 1 1 1 1
+9
+1 1 1 1 1 1 1 9 9
+9 9 9 9 9 9 1 9 9
+9 9 9 9 9 9 1 1 1
+9 9 9 9 9 9 1 1 1
+9 9 1 1 1 1 1 9 9
+9 9 1 9 9 9 9 9 9
+9 9 1 9 9 9 9 9 9
+9 9 1 9 9 9 9 9 9
+9 9 1 1 1 1 1 1 1
+"""
+import sys
+sys.stdin = open("c://Users//SSAFY//Desktop//YH//algo-python//PYTHON//boj//input.txt", "r")
+# sys.stdin = open("c://SSAFY//algo-python//PYTHON//boj//input.txt", "r")
