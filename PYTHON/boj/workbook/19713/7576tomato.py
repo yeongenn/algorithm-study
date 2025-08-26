3f26fc8f421aeda07f5656d4c4b83a02fc7b2eaf
yeongenn <yngn0012@naver.com>
2025-08-26 17:59:56 +0900

    boj 7576 토마토 풀이 중

---
+++ PYTHON/boj/workbook/19713/7576tomato.py
@@ -0,0 +1,4 @@
+import sys
+# sys.stdin = open("C:\\Users\\SSAFY\\Desktop\\YH\\algo-python\\PYTHON\\boj\\input.txt", "r")
+sys.stdin = open("C:\\Users\\SSAFY\\Desktop\\yeongenn\\algorithm-study\\PYTHON\\boj\\input.txt", "r")
+
